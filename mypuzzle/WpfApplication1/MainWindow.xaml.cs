e6da9958d522c86fd7c9b34d72ce124e4b041d1e
oscart820102 <102703028@nccu.edu.tw>
2015-06-01 19:37:38 +0800

    手動上色

--- mypuzzle/WpfApplication1/MainWindow.xaml.cs
+++ mypuzzle/WpfApplication1/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ using System.Runtime.InteropServices;
 
 namespace WpfApplication1
 {
-    //i
     /// <summary>
     /// MainWindow.xaml 的互動邏輯
     /// </summary>
@@ -32,50 +31,58 @@ namespace WpfApplication1
     {
            // timer顯示座標
             DispatcherTimer timer,timersec;
-           
-            //
+            bool changemode = false;             //手動變換珠子顏色 判斷是不是轉珠模式
+            int wood = 0, water = 0, fire = 0, gold = 0, dark = 0, heart = 0;
+
         public MainWindow()
         {
               // timer顯示座標
             DispatcherTimer timer,timersec;
             timer = new DispatcherTimer();
             timer.Tick += timer_1Tick;
-            
-          
             timer.Start();
-            //
-          
+            //          
             InitializeComponent();
             creatpuzzle(5,6);               
         }
-
-        void timersec_Tick(object sender, EventArgs e)
-        {
-            this.lbtimer.Content = (int.Parse(this.lbtimer.Content.ToString()) + 1).ToString();
-            //if (int.Parse(this.lbtimer.Content.ToString()) == 5) {
-            //    this.lbtimer.Content = "0";
-                
-            //    //this.lbtimer.Content = "0";
-            //}
+        
+        private void creatpuzzle(int x, int y) //動態生成珠子
+        {          
+            wood = 0; water = 0; fire = 0; gold = 0; dark = 0; heart = 0;
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    System.Windows.Shapes.Rectangle rec = new System.Windows.Shapes.Rectangle()
+                    {
+                        Height = 50,
+                        Width = 50,
+                        Stroke= new SolidColorBrush(Colors.Black),                      
+                    };
+                    randomfill(rec);    //隨機給珠子顏色           
+                    rec.MouseDown +=  new MouseButtonEventHandler(this.Rectangle_MouseDown); 
+                    rec.MouseMove +=  new MouseEventHandler(this.Rectangle_MouseMove);
+                    rec.MouseUp += new MouseButtonEventHandler(this.Rectangle_MouseUp);
+                    Canvas.SetTop(rec, 40 +i*55);
+                    Canvas.SetLeft(rec, 40+ j*55);                    
+                    can1.Children.Add(rec);
+                }
+            }
+            lbdark.Content = "暗珠: " + dark + "顆";
+            lbwater.Content = "水珠: " + water + "顆";
+            lbfire.Content = "火珠: " + fire + "顆";
+            lbwood.Content = "木珠: " + wood + "顆";
+            lbgold.Content = "光珠: " + gold + "顆";
+            lbheart.Content = "心珠: " + heart + "顆";
+            lbcombo.Content = "最大combo數: " + ((dark / 3) + (water / 3) + (fire / 3) + (wood / 3) + (gold / 3) + (heart / 3));
         }
 
-        private void timer_1Tick(object sender, EventArgs e)
+        private void randomfill(System.Windows.Shapes.Rectangle rec)//隨機上色
         {
-
-            this.plabel.Content = "(" + System.Windows.Forms.Cursor.Position.X.ToString() + "," + System.Windows.Forms.Cursor.Position.Y.ToString() + ")";
-           
-        }
-
-       
-        private void randomfill(System.Windows.Shapes.Rectangle rec) {
             Random rnd = new Random(Guid.NewGuid().GetHashCode());
             int x = rnd.Next(6);
-        
-            ImageBrush brush = null ;
-            //問助教為什麼要用image 不能用new的   ImageBrush brush = new ImageBrush(new BitmapImage(new Uri("images/gold.png", UriKind.Relative)));
-
-      
-            switch (x) { 
+            ImageBrush brush = null;
+            //問助教為什麼要用image 不能用new的   ImageBrush brush = new ImageBrush(new BitmapImage(new Uri("images/gold.png", UriKind.Relative)));      
+            switch (x)
+            {
                 case 1:
                     image1.Source = new BitmapImage(new Uri("images/gold.png", UriKind.Relative));
                     brush = new ImageBrush(image1.Source);
@@ -99,8 +106,8 @@ namespace WpfApplication1
                 case 5:
                     image1.Source = new BitmapImage(new Uri("images/wood.png", UriKind.Relative));
                     brush = new ImageBrush(image1.Source);
-                     wood++;
-                     break;
+                    wood++;
+                    break;
                 case 0:
                     image1.Source = new BitmapImage(new Uri("images/heart.png", UriKind.Relative));
                     brush = new ImageBrush(image1.Source);
@@ -110,116 +117,74 @@ namespace WpfApplication1
                     MessageBox.Show("no puzzle!!!!!");
                     break;
             }
-         //   ImageBrush brush = new ImageBrush(new BitmapImage(new Uri("picture/gold.png", UriKind.Relative)));
+            //   ImageBrush brush = new ImageBrush(new BitmapImage(new Uri("picture/gold.png", UriKind.Relative)));
             rec.Fill = brush;
-           
-            return ;
+            return;
         }
-        
-        private void creatpuzzle(int x, int y) 
-        {
-            wood = 0; water = 0; fire = 0; gold = 0; dark = 0; heart = 0;
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < y; j++) {
-                    System.Windows.Shapes.Rectangle rec = new System.Windows.Shapes.Rectangle()
-                    {
-                        Height = 50,
-                        Width = 50,
-                        Stroke= new SolidColorBrush(Colors.Black),
-                      
-                    };
-                    randomfill(rec);
-               
-                    rec.MouseDown +=  new MouseButtonEventHandler(this.Rectangle_MouseDown); 
-                    rec.MouseMove +=  new MouseEventHandler(this.Rectangle_MouseMove);
-                    rec.MouseUp += new MouseButtonEventHandler(this.Rectangle_MouseUp);
-                    Canvas.SetTop(rec, 40 +i*55);
-                    Canvas.SetLeft(rec, 40+ j*55);
-                    
-                    can1.Children.Add(rec);
-                }
-            }
-            lbdark.Content = "暗珠: " + dark + "顆";
-            lbwater.Content = "水珠: " + water + "顆";
-            lbfire.Content = "火珠: " + fire + "顆";
-            lbwood.Content = "木珠: " + wood + "顆";
-            lbgold.Content = "光珠: " + gold + "顆";
-            lbheart.Content = "心珠: " + heart + "顆";
-            lbcombo.Content = "最大combo數: " + ((dark / 3) + (water / 3) + (fire / 3) + (wood / 3) + (gold / 3) + (heart / 3));
-        }
-
-        int wood = 0, water = 0, fire = 0, gold = 0,dark=0,heart=0;
-
-        //mouse test
-        double mOldX;
-        double mOldY;
-        double mNewX;
-        double mNewY;
-        //
 
         double mouseX;
         double mouseY;
-        double oldpositionX;    //add by me
-        double oldpositionY;    // add by me
-        double currentshapX;
+        double oldpositionX;    //圖形原本位置
+        double oldpositionY;    
+        double currentshapX;    //圖形隨著滑鼠移動的位置
         double currentshapY;
-        System.Windows.Shapes.Rectangle CurrentRec = null;
+        System.Windows.Shapes.Rectangle CurrentRec = null;  //宣告目前移動的正方形
 
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            //timer
-            timersec = new DispatcherTimer();
-            timersec.Interval = TimeSpan.FromSeconds(1.0);
-            timersec.Tick += timersec_Tick;
-            timersec.Start();
-            //
-            System.Windows.Shapes.Rectangle item = sender as System.Windows.Shapes.Rectangle;
-            CurrentRec = item;
-           item.Fill = new SolidColorBrush(Colors.Red);  //delete later, just for mouse test
-            //mouse test
-            mOldX = System.Windows.Forms.Cursor.Position.X;
-            mOldY = System.Windows.Forms.Cursor.Position.Y;
-            //
 
-            Canvas.SetZIndex(item, 100);  //移動的物體總是在最上方
-            mouseX = e.GetPosition(null).X;
-            mouseY = e.GetPosition(null).Y;
-            item.CaptureMouse();
-            oldpositionX = (double)item.GetValue(Canvas.LeftProperty);
-            oldpositionY = (double)item.GetValue(Canvas.TopProperty);
-            lb1.Content = oldpositionX;
-            lb2.Content = oldpositionY;
+            if (changemode)  //換色模式
+            {
+                System.Windows.Shapes.Rectangle item = sender as System.Windows.Shapes.Rectangle;
+               // item.Fill = new SolidColorBrush(Colors.Red);  //delete later, just for mouse test
+                ImageBrush brush = null;
+                image1.Source = new BitmapImage(new Uri("images/dark.png", UriKind.Relative));
+                brush = new ImageBrush(image1.Source);
+                item.Fill = brush;
+            }
+            else  //轉珠模式
+            {
+                //timer
+                timersec = new DispatcherTimer();
+                timersec.Interval = TimeSpan.FromSeconds(1.0);
+                timersec.Tick += timersec_Tick;
+                timersec.Start();
+                //
+                System.Windows.Shapes.Rectangle item = sender as System.Windows.Shapes.Rectangle;
+                CurrentRec = item;  //記錄下要移動的正方形(hittest才可以判斷)
+                // item.Fill = new SolidColorBrush(Colors.Red);  //delete later, just for mouse test
+                Canvas.SetZIndex(item, 100);  //移動的物體總是在最上方
+                mouseX = e.GetPosition(null).X;
+                mouseY = e.GetPosition(null).Y;
+                item.CaptureMouse();
+                oldpositionX = (double)item.GetValue(Canvas.LeftProperty);
+                oldpositionY = (double)item.GetValue(Canvas.TopProperty);
+                lb1.Content = oldpositionX;
+                lb2.Content = oldpositionY;
+            }
+            
         }
 
         private void Rectangle_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            
 
-            System.Windows.Shapes.Rectangle item = sender as System.Windows.Shapes.Rectangle;
-            item.ReleaseMouseCapture();
-            Canvas.SetZIndex(item, 0);  //物體位置回歸水平
-            mouseX = -1;
-            mouseY = -1;
-            item.SetValue(Canvas.LeftProperty, oldpositionX); //轉完自動歸位圓圈
-            item.SetValue(Canvas.TopProperty, oldpositionY);  //
-
-            //mouse test
-        
-            mNewX = System.Windows.Forms.Cursor.Position.X;
-            mNewY = System.Windows.Forms.Cursor.Position.Y;
-            //
-
+            if (!changemode)  //如果是上色模式就不執行
+            {
+                System.Windows.Shapes.Rectangle item = sender as System.Windows.Shapes.Rectangle;
+                item.ReleaseMouseCapture();
+                Canvas.SetZIndex(item, 0);  //物體位置回歸水平
+                mouseX = -1;
+                mouseY = -1;
+                item.SetValue(Canvas.LeftProperty, oldpositionX); //轉完自動歸位圓圈
+                item.SetValue(Canvas.TopProperty, oldpositionY);  //  
+            }
         }
 
-
         private void Rectangle_MouseMove(object sender, MouseEventArgs e)
-        {
+        {                       
             System.Windows.Shapes.Rectangle item = sender as System.Windows.Shapes.Rectangle;
             if (item.IsMouseCaptured)
             {
-         
-
-
                 // Calculate the current position of the object.
                 double deltaX = e.GetPosition(null).X - mouseX;
                 double deltaY = e.GetPosition(null).Y - mouseY;
@@ -262,7 +227,6 @@ namespace WpfApplication1
                     oldpositionY = tempY;
                     lb1.Content = oldpositionX;
                     lb2.Content = oldpositionY;
-
                 }            
             }
             return HitTestResultBehavior.Continue;
@@ -270,74 +234,39 @@ namespace WpfApplication1
 
         private void btrand_Click(object sender, RoutedEventArgs e)
         {
-            can1.Children.RemoveRange(16,33); //從第10個產生的物件開始移除(移除拼圖)
+            can1.Children.RemoveRange(18,35); //從第18個產生的物件開始移除(移除拼圖)
             creatpuzzle(5, 6);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void btmove_Click(object sender, RoutedEventArgs e)
+        {           
             Mouse.MoveTo(tbx.Text, tby.Text);
-            Mouse.LeftClick();
-
-       //     Mouse.DragTo(mOldX.ToString(), mOldY.ToString(), mNewX.ToString(), mNewY.ToString());
+            Mouse.LeftClick();           
         }
 
-        /*       
-    private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
-    {
-           
-        Ellipse item = sender as Ellipse;
-        Canvas.SetZIndex(item, 100);  //移動的物體總是在最上方
-        mouseX = e.GetPosition(null).X;
-        mouseY = e.GetPosition(null).Y;
-        item.CaptureMouse();
-        oldpositionX = (double)item.GetValue(Canvas.LeftProperty);
-        oldpositionY = (double)item.GetValue(Canvas.TopProperty);
-        lb1.Content = oldpositionX;
-        lb2.Content = oldpositionY;
-    }
-    private void Ellipse_MouseUp(object sender, MouseButtonEventArgs e)
-    {
-        Ellipse item = sender as Ellipse;
-        Canvas.SetZIndex(item, 0);  //物體位置回歸水平
-        item.ReleaseMouseCapture();
-        mouseX = -1;
-        mouseY = -1;
-        item.SetValue(Canvas.LeftProperty, oldpositionX); //轉完自動歸位圓圈
-        item.SetValue(Canvas.TopProperty, oldpositionY);  //
-    }
-    private void Ellipse_MouseMove(object sender, MouseEventArgs e)
-    {
-        Ellipse item = sender as Ellipse;
-        if (item.IsMouseCaptured)
+        private void btchange_color_Click(object sender, RoutedEventArgs e)
         {
-            // Calculate the current position of the object.
-            double deltaX = e.GetPosition(null).X - mouseX;
-            double deltaY = e.GetPosition(null).Y - mouseY;
-            double newLeft = deltaX + (double)item.GetValue(Canvas.LeftProperty);
-            double newTop = deltaY + (double)item.GetValue(Canvas.TopProperty);
-            // Set new position of object.
-            item.SetValue(Canvas.LeftProperty, newLeft);
-            item.SetValue(Canvas.TopProperty, newTop);             
-            //add by me
-            currentshapX = (double)item.GetValue(Canvas.LeftProperty);
-            currentshapY = (double)item.GetValue(Canvas.TopProperty);
-            // Update position global variables.
-            mouseX = e.GetPosition(null).X;
-            mouseY = e.GetPosition(null).Y;
-            //將圓形圖轉成幾何圖形
-            Geometry g = item.RenderedGeometry;
-            //座標位置轉換為視窗的座標
-            g.Transform = item.TransformToAncestor(this) as MatrixTransform;
-            VisualTreeHelper.HitTest(this, null,
-                new HitTestResultCallback(myHitTestResult),
-                new GeometryHitTestParameters(g));
+            changemode = true;
         }
-    }
 
-    */
+        private void btspin_mode_Click(object sender, RoutedEventArgs e)
+        {
+            changemode = false;
+        }
 
+        void timersec_Tick(object sender, EventArgs e)
+        {
+            this.lbtimer.Content = (int.Parse(this.lbtimer.Content.ToString()) + 1).ToString();
+            //if (int.Parse(this.lbtimer.Content.ToString()) == 5) {
+            //    this.lbtimer.Content = "0";               
+            //    //this.lbtimer.Content = "0";
+            //}
+        }
 
+        private void timer_1Tick(object sender, EventArgs e)
+        {
+            this.plabel.Content = "(" + System.Windows.Forms.Cursor.Position.X.ToString() + "," + System.Windows.Forms.Cursor.Position.Y.ToString() + ")";
+        }
     }
 
     //add for mouse
@@ -384,63 +313,16 @@ namespace WpfApplication1
         public const int GCS_COMPSTR = 0x0008;
     }
 
-    //public partial class Form1 : Form
-    //{
-    //    public Form1()
-    //    {
-    //        InitializeComponent();
-    //    }
-
-    //    private void button1_Click(object sender, EventArgs e)
-    //    {
-    //        Thread.Sleep(500);
-    //        Mouse.MoveTo(tX1.Text, tY1.Text);
-    //    }
-
-    //    private void button4_Click(object sender, EventArgs e)
-    //    {
-    //        Thread.Sleep(500);
-    //        Mouse.MoveTo(tX1.Text, tY1.Text);
-    //        Mouse.LeftClick();
-    //    }
-
-    //    private void button2_Click(object sender, EventArgs e)
-    //    {
-    //        Thread.Sleep(500);
-    //        Mouse.MoveTo(tX1.Text, tY1.Text);
-    //        // Mouse.LeftDoubleClick();
-    //    }
-
-    //    private void button3_Click(object sender, EventArgs e)
-    //    {
-    //        Thread.Sleep(500);
-    //        Mouse.DragTo(tX1.Text, tY1.Text, tX2.Text, tY2.Text);
-    //    }
-
-    //    private void timer1_Tick(object sender, EventArgs e)
-    //    {
-    //        this.Text = "(" + Cursor.Position.X.ToString() + "," + Cursor.Position.Y.ToString() + ")";
-    //    }
-    //}
-
     public static partial class Mouse
     {
-
         [DllImport("User32")]
-
-
         public extern static void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
-
-
-
         static public void DragTo(string sor_X, string sor_Y, string des_X, string des_Y)
         {
             MoveTo(sor_X, sor_Y);
-          //  Thread.Sleep(1200);
             LeftDown();
             Thread.Sleep(200);
             MoveTo(des_X, des_Y);
-     //       Thread.Sleep(200);
             LeftUp();
         }
         /// <summary>
@@ -452,10 +334,7 @@ namespace WpfApplication1
             int x, y;
             int.TryParse(tx, out x);
             int.TryParse(ty, out y);
-
-
             System.Windows.Forms.Cursor.Position = new System.Drawing.Point(x, y);
-            //    Cursor.Position = new Point(x, y);
         }
 
         public static void LeftDown()
@@ -500,8 +379,6 @@ namespace WpfApplication1
             RightDown();
             RightUp();
         }
-
-
     }
 
 
